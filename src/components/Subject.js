2fa8e2771a2785ccab48330a8a439feb9767a487
Juin Chiu <davidjuin0519@gmail.com>
2019-12-20 23:16:25 +0800

    Use jszip to extract downloaded snark data

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -1,21 +1,56 @@
 import React, { Component } from 'react';
 import { Card, Button, Col } from 'react-bootstrap';
 import { endpoint } from '../env';
+import { generateProof } from '../zkp/proof_generation';
+import jsZip from 'jszip';
 
 class Subject extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      cir_def: {},
+      proving_key: {},
+      verification_key: {}
+    }
 
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleJoin = this.handleJoin.bind(this);
+    this.handleVote = this.handleVote.bind(this);
   }
 
-  handleSubmit(event) {
+  componentDidMount() {
+    // Download snark data
+    let url = new URL(endpoint + "/identities/snark_data");
+    let component = this
+
+    fetch(url)
+    .then(res => res.blob())
+    .then(
+      (result) => {
+        jsZip.loadAsync(result).then(function (zip) {
+          zip.files["snark_data/verification_key.json"].async('string').then(fileData => component.setState({ verification_key: fileData }))
+          zip.files["snark_data/proving_key.json"].async('string').then(fileData => component.setState({ proving_key: fileData }))
+          zip.files["snark_data/circuit.json"].async('string').then(fileData => component.setState({ cir_def: fileData }))
+        })
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
+  }
+
+  handleJoin(event) {
     event.preventDefault();
 
     const data = new FormData();
 
     if (window.zkvote) {
-      data.set("identityCommiement", window.zkvote.identityCommitment)
+      data.set("identityCommitment", window.zkvote.identityCommitment)
       data.set("subjectHash", this.props.subjectHash)
 
       fetch(endpoint + "/subjects/join", {
@@ -25,6 +60,47 @@ class Subject extends Component {
     } else {
       console.error("identity commitment is not set yet")
     }
+  }
+
+  handleVote (event) {
+    event.preventDefault();
+
+    // Get identity path first
+    let url = new URL(endpoint + "/subjects/identity_path");
+    url.search = new URLSearchParams({
+      subjectHash: this.props.subjectHash,
+      identityCommitment: window.zkvote.identityCommitment
+    }).toString();
+
+    fetch(url)
+    .then(res => res.json())
+    .then(
+      (result) => {
+        console.log(result)
+        console.log(this.state.cir_def)
+        console.log(this.state.proving_key)
+        console.log(this.state.verification_key)
+        // Generate Proof
+        // generateProof(
+        //   this.state.cir_def,
+        //   this.state.proving_key,
+        //   this.state.verification_key,
+        //   window.zkvote.secret,
+        //   identity_path,
+        //   "this is a question.",
+        //   i%2
+        // )
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      }
+    )
   }
 
   render() {
@@ -42,9 +118,9 @@ class Subject extends Component {
             <Card.Text>
               Subjech Hash: {this.props.subjectHash}
               <br/>
-              <Button variant="primary" onClick={this.handleSubmit}>Join</Button>
+              <Button variant="primary" onClick={this.handleJoin}>Join</Button>
             </Card.Text>
-            <Button style={{ margin: '5px' }} variant="primary">Vote</Button>
+            <Button style={{ margin: '5px' }} variant="primary" onClick={this.handleVote}>Vote</Button>
             {/* <Card.Link href="#">Register</Card.Link>
             <Card.Link href="#">Vote</Card.Link> */}
           </Card.Body>
