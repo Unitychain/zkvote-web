31b7e2807373104b85931057dbfe525d62287f7c
Juin Chiu <davidjuin0519@gmail.com>
2019-12-24 18:01:34 +0800

    Implement open

--- src/components/Subject.js
+++ src/components/Subject.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { Card, Button, Col } from 'react-bootstrap';
 import { endpoint } from '../env';
 import { generateProof } from '../zkp/proof_generation';
-import jsZip from 'jszip';
 import snarkjs from 'snarkjs';
 const bigInt = snarkjs.bigInt;
 
@@ -10,45 +9,16 @@ class Subject extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      cir_def: {},
-      proving_key: {},
-      verification_key: {},
-      has_snark_data: {
-        verification_key: false,
-        proving_key: false,
-        cir_def: false
+      open: {
+        yes: 0,
+        no: 0
       }
     }
 
     this.handleJoin = this.handleJoin.bind(this);
     this.handleVote = this.handleVote.bind(this);
-  }
-
-  componentDidMount() {
-    // Download snark data
-    let url = new URL(endpoint + "/identities/snark_data");
-    let component = this
-
-    fetch(url)
-    .then(res => res.blob())
-    .then(
-      (result) => {
-        jsZip.loadAsync(result).then(function (zip) {
-          zip.files["snark_data/verification_key.json"].async('string').then(fileData => component.setState({ verification_key: JSON.parse(fileData), has_snark_data: { ...component.state.has_snark_data, verification_key: true }}))
-          zip.files["snark_data/proving_key.json"].async('string').then(fileData => component.setState({ proving_key: JSON.parse(fileData), has_snark_data: { ...component.state.has_snark_data, proving_key: true }}))
-          zip.files["snark_data/circuit.json"].async('string').then(fileData => component.setState({ cir_def: JSON.parse(fileData), has_snark_data: { ...component.state.has_snark_data, cir_def: true }}))
-        })
-      },
-      // Note: it's important to handle errors here
-      // instead of a catch() block so that we don't swallow
-      // exceptions from actual bugs in components.
-      (error) => {
-        this.setState({
-          isLoaded: true,
-          error
-        });
-      }
-    )
+    this.handleVoteDebug = this.handleVoteDebug.bind(this);
+    this.handleOpen = this.handleOpen.bind(this);
   }
 
   handleJoin(event) {
@@ -56,8 +26,8 @@ class Subject extends Component {
 
     if (window.zkvote) {
       const data = new FormData();
-      data.set("identityCommitment", window.zkvote.identityCommitment)
       data.set("subjectHash", this.props.subjectHash)
+      data.set("identityCommitment", window.zkvote.identityCommitment)
 
       fetch(endpoint + "/subjects/join", {
         method: 'POST',
@@ -72,9 +42,9 @@ class Subject extends Component {
     event.preventDefault();
 
     if (window.zkvote &&
-      this.state.has_snark_data.verification_key === true &&
-      this.state.has_snark_data.proving_key === true &&
-      this.state.has_snark_data.cir_def === true
+      this.props.has_snark_data.verification_key === true &&
+      this.props.has_snark_data.proving_key === true &&
+      this.props.has_snark_data.cir_def === true
       ) {
       // Get identity path first
       let url = new URL(endpoint + "/subjects/identity_path");
@@ -85,51 +55,80 @@ class Subject extends Component {
 
       fetch(url)
       .then(res => res.json())
-      .then(
-        res => {
-          let identity_path =  {
-            "path_elements": res.results.path.map(p => bigInt(p)),
-            "path_index": res.results.index,
-            "root": bigInt(res.results.root)
-          }
+      .then(res => {
+        let identity_path =  {
+          "path_elements": res.results.path.map(p => bigInt(p)),
+          "path_index": res.results.index,
+          "root": bigInt(res.results.root)
+        }
 
-          // Generate Proof
-          // This will take ~10 minutess
-          let zkvote_proof = generateProof(
-            this.state.cir_def,
-            this.state.proving_key,
-            this.state.verification_key,
-            window.zkvote.secret,
-            identity_path,
-            "this is a question.",
-            0
-          )
+        // Generate Proof
+        // This will take ~10 minutess
+        let zkvote_proof = generateProof(
+          this.props.cir_def,
+          this.props.proving_key,
+          this.props.verification_key,
+          window.zkvote.secret,
+          identity_path,
+          this.props.subjectHash,
+          0
+        )
 
-          let proof = JSON.stringify(zkvote_proof.proof)
-          console.log(proof)
+        // console.log(JSON.stringify(zkvote_proof.root))
+        // console.log(JSON.stringify(zkvote_proof.nullifier_hash))
+        // console.log(JSON.stringify(zkvote_proof.proof))
+        // console.log(JSON.stringify(zkvote_proof.public_signal))
 
-          // Sumbit proof
-          const data = new FormData();
-          data.set("proof", proof)
+        // Sumbit proof
+        const data = new FormData();
+        data.set("subjectHash", this.props.subjectHash)
+        data.set("proof", JSON.stringify(zkvote_proof))
 
-          fetch(endpoint + "/subjects/vote", {
-            method: 'POST',
-            body: data,
-          });
-        },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
-        (error) => {
-          this.setState({
-            isLoaded: true,
-            error
-          });
-        })
+        fetch(endpoint + "/subjects/vote", {
+          method: 'POST',
+          body: data,
+        });
+      },
+      // Note: it's important to handle errors here
+      // instead of a catch() block so that we don't swallow
+      // exceptions from actual bugs in components.
+      (error) => {
+        this.setState({
+          isLoaded: true,
+          error
+        });
+      })
     } else {
-      console.log(this.state.has_snark_data)
+      console.log(this.props.has_snark_data)
       console.error("identity commitment is not set yet or snark data is not loaded yet")
     }
+  }
+
+  handleVoteDebug() {
+    let zkvote_proof= {"root":"11987934088855946922120596363647040988335768868904013217674253507824621847250","nullifier_hash":"10904379290953876053387344218603877561403285588622727294496925276926330821634","proof":{"pi_a":["19392027185010717843627025669629081194066013769406217517531644951222831749008","17928270861769032259665484821862320799312498646032118903080617742428774978435","1"],"pi_b":[["7375329943748789156784527025465416837037719471670233273022164053523559291879","9431361778638170377374981918977114105094189631279641098602737823129268069965"],["5977416295975762676440189094224556048921852465526490951103848135535353746930","1197436965527130354872596760601105014694309088437948608917391057431790819338"],["1","0"]],"pi_c":["14876480443851799145283871711796892415045018824113442423733059396179414149177","17515520857584347264376304941059551013905752380483643376320063464589917697346","1"],"protocol":"groth"},"public_signal":["11987934088855946922120596363647040988335768868904013217674253507824621847250","10904379290953876053387344218603877561403285588622727294496925276926330821634","43379584054787486383572605962602545002668015983485933488536749112829893476306","5223042267517509412808214028492453981166929943990137203839434558324753558041"]}
+
+    // Sumbit proof
+    const data = new FormData();
+    data.set("subjectHash", this.props.subjectHash)
+    data.set("proof", JSON.stringify(zkvote_proof))
+
+    fetch(endpoint + "/subjects/vote", {
+      method: 'POST',
+      body: data,
+    });
+  }
+
+  handleOpen() {
+    let url = new URL(endpoint + "/subjects/open");
+    url.search = new URLSearchParams({
+      subjectHash: this.props.subjectHash
+    }).toString();
+
+    fetch(url)
+    .then(res => res.json())
+    .then(res => {
+      this.setState({ open: res.results })
+    })
   }
 
   render() {
@@ -146,10 +145,14 @@ class Subject extends Component {
             </Card.Text>
             <Card.Text>
               Subjech Hash: {this.props.subjectHash}
-              <br/>
-              <Button variant="primary" onClick={this.handleJoin}>Join</Button>
             </Card.Text>
-            <Button style={{ margin: '5px' }} variant="primary" onClick={this.handleVote}>Vote</Button>
+            <Card.Text>
+              Yes: {this.state.open.yes}, No: {this.state.open.no}
+            </Card.Text>
+            <Button style={{ margin: '2px' }} variant="primary" onClick={this.handleJoin}>Join</Button>
+            <Button style={{ margin: '2px' }} variant="primary" onClick={this.handleVote}>Vote</Button>
+            {/* <Button style={{ margin: '2px' }} variant="primary" onClick={this.handleVoteDebug}>Vote</Button> */}
+            <Button style={{ margin: '2px' }} variant="primary" onClick={this.handleOpen}>Open</Button>
             {/* <Card.Link href="#">Register</Card.Link>
             <Card.Link href="#">Vote</Card.Link> */}
           </Card.Body>
